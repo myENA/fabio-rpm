6bc3620ff6904586ad60b0cd910c83296a08101a
Nathan Johnson <njohnson@ena.com>
2022-04-12 10:20:09 -0500

    removing interactive on docker run command

--- dockerbuilder.sh
+++ dockerbuilder.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build -t redlinebuilder7 -f Dockerfile7 .
-docker run -v $PWD:/tmp/build -it redlinebuilder7
+docker run -v $PWD:/tmp/build -t redlinebuilder7
 
 docker build -t redlinebuilder8 -f Dockerfile8 .
-docker run -v $PWD:/tmp/build -it redlinebuilder8
+docker run -v $PWD:/tmp/build -t redlinebuilder8
