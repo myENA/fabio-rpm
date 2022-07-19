96f08798e350f708c8dc35507ec450909fb134b0
Nathan Johnson <njohnson@ena.com>
2022-07-19 15:12:15 -0500

    update the builder

--- dockerbuilder.sh
+++ dockerbuilder.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+docker pull centos:7
+
 docker build -t redlinebuilder7 -f Dockerfile7 .
 docker run -v $PWD:/tmp/build redlinebuilder7
 
+docker pull almalinux:8
 docker build -t redlinebuilder8 -f Dockerfile8 .
 docker run -v $PWD:/tmp/build redlinebuilder8
