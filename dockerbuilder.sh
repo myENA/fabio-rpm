2aaa1ac398c61dbbdc19b4a064069d15dd652ee4
Nathan Johnson <njohnson@ena.com>
2022-11-04 10:06:16 -0500

    updating to fabio 1.6.2

--- dockerbuilder.sh
+++ dockerbuilder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-docker pull centos:7
+docker pull --platform linux/x86_64 centos:7
 
-docker build -t redlinebuilder7 -f Dockerfile7 .
-docker run -v $PWD:/tmp/build redlinebuilder7
+docker build --platform linux/x86_64 -t redlinebuilder7 -f Dockerfile7 .
+docker run --platform linux/x86_64 -v $PWD:/tmp/build redlinebuilder7
 
-docker pull almalinux:8
-docker build -t redlinebuilder8 -f Dockerfile8 .
-docker run -v $PWD:/tmp/build redlinebuilder8
+docker pull --platform linux/x86_64 almalinux:8
+docker build  --platform linux/x86_64 -t redlinebuilder8 -f Dockerfile8 .
+docker run  --platform linux/x86_64 -v $PWD:/tmp/build redlinebuilder8
